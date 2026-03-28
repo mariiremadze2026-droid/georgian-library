fc61a0f88673f38dedaf1909b56de80edc9636da
mariiremadze2026-droid <mari.iremadze2026@icloud.com>
2026-03-28 05:03:57 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,5 @@
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("library-cache").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "style.css",
-        "script.js"
-      ]);
-    })
-  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
-});
+self.addEventListener("fetch", e => {});
